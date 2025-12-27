cdd45046a2602fa525a71cb4816bf5260d70e8e2
Rayan Trad <rayan.trad2005@gmail.com>
2025-12-28 00:56:23 +0200

    Change port connection

--- products/server.js
+++ products/server.js
@@ -618,6 +618,7 @@ app.delete("/orders/:id", (req, res) => {
 
 
 
-app.listen(5000, () => {
-  console.log("Backend server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
 });
