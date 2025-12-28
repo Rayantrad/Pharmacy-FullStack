342b15e95cb6081d831ab3e58adeaa0454e3e0d2
Rayan Trad <rayan.trad2005@gmail.com>
2025-12-28 15:49:45 +0200

    Change the connection2

--- products/server.js
+++ products/server.js
@@ -173,13 +173,14 @@ app.use(express.json());
 
 // Create connection using environment variables
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      
-  user: process.env.DB_USER,       
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,   
-  port: process.env.DB_PORT || 3306,
-  
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306
+
 });
+
 
 db.connect(err => {
   if (err) {
